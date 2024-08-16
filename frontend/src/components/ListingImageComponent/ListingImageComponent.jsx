b204ce77e98adce47c66ad7f1443196a39715744
Harshita Mahajan <harshitamahajan2020@gmail.com>
2024-08-16 13:01:33 +0530

    ListingImageComponent

---
+++ frontend/src/components/ListingImageComponent/ListingImageComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./ListingImageComponent.css"
+
+const ListingImageComponent = () => {
+  return (
+    <div className='imageComp'>
+      <img className="ListingImageComp-img" src="https://ultra-realhomes.b-cdn.net/wp-content/uploads/2022/06/architecture-home-merrick-way.jpg" alt="" />
+    </div>
+  )
+}
+
+export default ListingImageComponent
